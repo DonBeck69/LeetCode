c62c150b81c66f6572c7a231a6f1e974f0784808
Donovan Beck <donovan.beck.69@outlook.com>
2020-10-06 18:35:23 -0700

    FindMode

---
+++ LeetCode/ModeInBinaryTree.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ModeInBinaryTree
+    {
+        public int[] FindMode2(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new int[0];
+            }
+            List<int> allResults = new List<int>();
+            List<int> results = new List<int>();
+            Dictionary<int, int> modes = new Dictionary<int, int>();
+            Ent(root, modes);
+            int max = 0;
+            foreach(KeyValuePair<int, int> mode in modes)
+            {
+                //max = mode.Value;
+                if (mode.Value == max)
+                {
+                    results.Add(mode.Key);
+                    max = mode.Value;
+                }
+                if (mode.Value > max)
+                {
+                    results.Clear();
+                    results.Add(mode.Key);
+                    max = mode.Value;
+                }
+
+            }
+            
+            return results.ToArray();
+
+        }
+
+        public int[] FindMode(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new int[0];
+            }
+            List<int> allResults = new List<int>();
+            List<int> results = new List<int>();
+            Dictionary<int, int> modes = new Dictionary<int, int>();
+            Ent(root, modes);
+            modes = modes.OrderBy(v => v.Value).ToDictionary(k => k.Key, v => v.Value);
+
+            results.Add(modes.Last().Key);
+            if (modes.Count > 1)
+            {
+                for (int i = modes.Count - 2; i >= 0; i--)
+                {
+                    if (modes.ElementAt(i).Value == modes.Last().Value)
+                    {
+                        results.Add(modes.ElementAt(i).Key);
+                    }
+                }
+            }
+
+            return results.ToArray();
+
+        }
+
+        public void Ent(TreeNode node, Dictionary<int, int> modes)
+        {
+            if (node != null)
+            {
+                if (modes.ContainsKey(node.val))
+                {
+                    modes[node.val]++;
+                }
+                else
+                {
+                    modes.Add(node.val, 1);
+                }
+
+                Ent(node.left, modes);
+                Ent(node.right, modes);
+                
+            }
+        }
+    }
+}
+
+
+ public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
---
+++ LeetCodeTests/ModeInBinaryTreeTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class ModeInBinaryTreeTests
+    {
+        [TestMethod()]
+        public void FindModeTest()
+        {
+            ModeInBinaryTree modeInBinaryTree = new ModeInBinaryTree();
+            TreeNode node2l = new TreeNode(2);
+            TreeNode node1r = new TreeNode(2, node2l, null);
+            TreeNode node = new TreeNode(1, null, node1r);
+            int[] res = modeInBinaryTree.FindMode2(node);
+            Assert.IsTrue(res[0] == 2);
+        }
+
+        [TestMethod()]
+        public void FindModeTest2()
+        {
+            ModeInBinaryTree modeInBinaryTree = new ModeInBinaryTree();
+            TreeNode node2l = new TreeNode(2);
+            TreeNode node1r = new TreeNode(2, node2l, null);
+            TreeNode node1l = new TreeNode(1, null, null);
+            TreeNode node = new TreeNode(1, node1l, node1r);
+            int[] res = modeInBinaryTree.FindMode2(node);
+            Assert.IsTrue(res[0] == 1);
+            Assert.IsTrue(res[1] == 2);
+        }
+
+        [TestMethod()]
+        public void FindModeTest3()
+        {
+            ModeInBinaryTree modeInBinaryTree = new ModeInBinaryTree();
+
+            TreeNode node = new TreeNode(2147483647);
+            int[] res = modeInBinaryTree.FindMode2(node);
+            Assert.IsTrue(res[0] == 2147483647);
+        }
+
+    }
+}
